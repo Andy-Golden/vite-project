29812f5ed6fafdea795732de0e46cc687e144a33
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-10-08 18:08:31 +0700

    feat: basic ui profile card

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import avatar from "./avatar.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="cards">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="card__avatar">
+          <img src={avatar} alt="avatar"></img>
+        </div>
+        <p className="card__user-name">Dinh Nam</p>
+        <p className="card__user-major">Fullstack developer</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
