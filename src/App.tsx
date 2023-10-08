5eb4e9b5b7e144a04d6ad0ae0ef9eec68f5cd0b1
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-10-08 19:33:10 +0700

    feat: transition and complete ui

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,23 @@ function App() {
         </div>
         <p className="card__user-name">Dinh Nam</p>
         <p className="card__user-major">Fullstack developer</p>
+        <div className="card__social-icons">
+          <a href="https://github.com/Andy-Golden/vite-project" target="_blank">
+            <i className="fab fa-facebook-f card__social-icon"></i>
+          </a>
+          <a href="https://github.com/Andy-Golden/vite-project" target="_blank">
+            <i className="fab fa-youtube card__social-icon"></i>
+          </a>
+          <a href="https://github.com/Andy-Golden/vite-project" target="_blank">
+            <i className="fab fa-tiktok card__social-icon"></i>
+          </a>
+          <a href="https://github.com/Andy-Golden/vite-project" target="_blank">
+            <i className="fab fa-github card__social-icon"></i>
+          </a>
+        </div>
+        <button className="card__btn-contact">
+          Contact me
+        </button>
       </div>
       <div className="card"></div>
       <div className="card"></div>
